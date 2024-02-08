9b711ac6498a5d7c3fb362c8f653fafa0cddd54e
Dannysib <daniel.sibrian@csuglobal.edu>
2024-02-08 01:30:34 -0700

    Starting loop application

---
+++ src/LoopingConstruct.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+//Write a program that utilizes a while-loop to read a set of five floating-point values from user input.
+// Include code to prevent an endless loop. Ask the user to enter the values, then print the following data:
+//
+//Total
+//Average
+//Maximum
+//Minimum
+//Interest on total at 20%
+public class LoopingConstruct {
+    public static void main(String[]args){
+        Scanner scn= new Scanner(System.in);
+        float input; // asks the user input
+        float tot;   //total of all 5 values
+        float avg;   //average- 5 / total
+        float max;   //maximum
+        float min;   //minimum
+        float interest; //interest on total 20% or total * .2
+        //collect the 5 values from user //
+        System.out.print("Please enter your five values: ");
+        input = scn.nextFloat();
+
+
+        }
+    }
+}
